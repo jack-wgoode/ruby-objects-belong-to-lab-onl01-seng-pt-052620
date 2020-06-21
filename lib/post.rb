c2964eb1d1beb8c713b45c8532b9b27f2ac9b12e
Jack Goode <jack.w.goode@icloud.com>
2020-06-21 15:20:34 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post  
+  attr_accessor :title, :author, :name 
+  
+  def initialize
+    self.title = title 
+    
+  end
+  
+  def author 
+    @author 
+  end
+  
+  
+  def title 
+    @title
+  end
+  
+  def title=(title)
+    @title = title 
+  end
+ 
+  
+end 
